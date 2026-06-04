793a7ef21c2d19e47989754592c880dfd53b18aa
mstrouss-newco <mstrouss@gmail.com>
2026-06-03 17:15:59 -0700

    Fix: use index key in rollupOptions so output is index.html not app.html

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-        base: '/demo/',
-        plugins: [react()],
-        build: {
-                    rollupOptions: {
-                                    input: {
-                                                        app: './index.html'
-                                    }
-                    }
-        }
+            base: '/demo/',
+            plugins: [react()],
+            build: {
+                            rollupOptions: {
+                                                input: {
+                                                                        index: './index.html'
+                                                }
+                            }
+            }
 })
